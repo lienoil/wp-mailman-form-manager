5fbc4a8a529b2fa6e4aa2f5d4786315a259b439b
John Lioneil Dionisio <john.dionisio1@gmail.com>
2016-11-29 11:40:37 +0800

    added files

---
+++ wp-mailman-form-manager.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: WP Mailman Form Manager
+ * Version: 1.0.0
+ * Plugin URI: https://github.com/lioneil/wp-mailman-form-manager
+ * Description: A simple form manager for your site.
+ * Author: John Lioneil Dionisio
+ */
+
